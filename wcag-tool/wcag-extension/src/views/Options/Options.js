3375c6ccbe864e10593a3a1f41076600aa1de2f3
Ruben Eppink <ruben_eppink@live.nl>
2021-11-13 15:31:36 +0100

    wcag-tool client and server setup with CI pipeline

---
+++ wcag-tool/wcag-extension/src/views/Options/Options.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useSettings } from "../../hooks";
+import "./options.css";
+
+const Options = function () {
+  const [
+    { setBorderColor, setBorderWidth, setEnableBorders },
+    { borderColor, borderWidth, enableBorders },
+  ] = useSettings();
+  const [color, setColor] = useState(borderColor);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>border width: {borderWidth}</p>
+        <button onClick={() => setBorderWidth(borderWidth - 1)}>
+          decrease
+        </button>
+        <button onClick={() => setBorderWidth(borderWidth + 1)}>
+          increase
+        </button>
+
+        <hr />
+
+        <p>border color: {borderColor}</p>
+        <input
+          type="text"
+          onChange={(event) => setColor(event.target.value)}
+          value={color}
+        />
+        <button onClick={() => setBorderColor(color)}>set color</button>
+
+        <hr />
+
+        <p>borders enabled: {enableBorders.toString()}</p>
+        <button onClick={() => setEnableBorders(!enableBorders)}>toggle</button>
+      </header>
+    </div>
+  );
+};
+
+export { Options };
