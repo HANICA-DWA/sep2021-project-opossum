07f3a3556250e645a0073763c88cdfc04070acc5
Iliass <iliass@iek.dev>
2021-11-16 15:35:26 +0100

    Add Redux

--- wcag-tool/chrome-extension/src/views/popup.jsx
+++ wcag-tool/chrome-extension/src/views/popup.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { setupStore } from '../services/store'
 
 const Popup = function () {
   return <h1>Popup</h1>
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Popup />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+;(async () => {
+  const store = await setupStore()
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Popup />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+})()
