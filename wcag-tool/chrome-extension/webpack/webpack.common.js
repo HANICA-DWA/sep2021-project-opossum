7acca0eb71959c8ea5f3730c068842aa8091c382
Iliass <iliass@iek.dev>
2021-11-16 10:47:22 +0100

    Fix ESLint errors...

--- wcag-tool/chrome-extension/webpack/webpack.common.js
+++ wcag-tool/chrome-extension/webpack/webpack.common.js
@@ -1,45 +1,47 @@
+// eslint-disable-next-line no-unused-vars
 const webpack = require('webpack')
 const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+
 const srcDir = path.join(__dirname, '..', 'src')
 
 module.exports = {
-  entry: {
-    popup: path.join(srcDir, 'popup.jsx'),
-    options: path.join(srcDir, 'options.jsx'),
-    background: path.join(srcDir, 'background.js'),
-    content_script: path.join(srcDir, 'content_script.jsx'),
-  },
-  output: {
-    path: path.join(__dirname, '../dist/js'),
-    filename: '[name].js',
-  },
-  optimization: {
-    splitChunks: {
-      name: 'vendor',
-      chunks(chunk) {
-        return chunk.name !== 'background'
-      },
+    entry: {
+        popup: path.join(srcDir, 'popup.jsx'),
+        options: path.join(srcDir, 'options.jsx'),
+        background: path.join(srcDir, 'background.js'),
+        content_script: path.join(srcDir, 'content_script.jsx'),
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    output: {
+        path: path.join(__dirname, '../dist/js'),
+        filename: '[name].js',
+    },
+    optimization: {
+        splitChunks: {
+            name: 'vendor',
+            chunks(chunk) {
+                return chunk.name !== 'background'
+            },
         },
-      },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: '.', to: '../', context: 'public' }],
+            options: {},
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: '.', to: '../', context: 'public' }],
-      options: {},
-    }),
-  ],
 }
