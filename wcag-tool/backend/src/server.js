da46be87fb0a4d4372bc8ecb7420495a0de3d45b
Ruben Eppink <ruben_eppink@live.nl>
2021-11-17 15:46:31 +0100

    basic server setup

--- wcag-tool/backend/src/server.js
+++ wcag-tool/backend/src/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const logger = require('./middleware');
 
 const app = express();
 const port = 3000;
+const mongoPort = 27017;
+const dbName = 'wcag-tool';
+
+app.use(cors({ origin: true, credentials: true }));
+app.options('*', cors({ origin: true, credentials: true }));
+app.use(logger);
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  mongoose.connect(
+    `mongodb://localhost:${mongoPort}/${dbName}`,
+    { useNewUrlParser: true },
+    () => {
+      console.log(`wcag-tool server started on port ${port}`);
+    }
+  );
 });
