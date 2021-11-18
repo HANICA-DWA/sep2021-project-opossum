cc460039b9a1b9fa15af49523c065eccddc250e1
Ruben Eppink <ruben_eppink@live.nl>
2021-11-18 17:36:53 +0100

    added getSnapshot middleware

--- wcag-tool/backend/src/middleware.js
+++ wcag-tool/backend/src/middleware.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Snapshot = mongoose.model('Snapshot');
+
 exports.logger = (req, res, next) => {
   const { method } = req.method;
   const url = req.originalUrl;
@@ -12,4 +15,20 @@ exports.errorHandler = (err, req, res, next) => {
   console.log('error: ', err);
   const code = err.code || 500;
   res.status(code).send(err);
+};
+
+// sets snapshot on the request object
+exports.getSnapshot = async (req, res, next) => {
+  if (req.params.snapshotId) {
+    try {
+      const snapshot = await Snapshot.findById(req.params.snapshotId);
+      if (!snapshot) {
+        return next({ code: 400, message: 'Deze snapshot bestaat niet' });
+      }
+      req.snapshot = snapshot;
+    } catch (err) {
+      next(err);
+    }
+  }
+  return next();
 };
