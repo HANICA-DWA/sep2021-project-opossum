7b3645781f1a6cacad05c7f4a4d79a86b80d5286
Steve <stefanoudelohuis2000@gmail.com>
2021-11-19 10:00:43 +0100

    snapshot v0.3

--- wcag-tool/chrome-extension/src/services/snapshotSlice.js
+++ wcag-tool/chrome-extension/src/services/snapshotSlice.js
@@ -1,18 +1,20 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const fetchMessage = createAsyncThunk('/message', async ({ rejectWithValue }) => {
-  try {
-    const res = await axios.get(`http://localhost:3001/message`)
-    // console.log(res.data)
-    return res.data
+const fetchMessage = createAsyncThunk(
+  '/message',
+  async ({ rejectWithValue }) => {
+    try {
+      const res = await axios.get(`http://localhost:3001/message`)
+      // console.log(res.data)
+      return res.data
+    } catch (error) {
+      if (!error.response) throw error
+
+      return rejectWithValue(error.response.data)
+    }
   }
-  catch (error) {
-    if (!error.response) throw error
-    
-    return rejectWithValue(error.response.data)
-  }
-})
+)
 
 const snapshotSlice = createSlice({
   name: 'snapshot',
--- wcag-tool/chrome-extension/src/views/options.jsx
+++ wcag-tool/chrome-extension/src/views/options.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+// eslint-disable-next-line import/named
 import { setupStore } from '../services/store'
 
+// eslint-disable-next-line func-names
 const Options = function () {
-  return <h1>Options</h1>
+  return (
+    <div className="header">
+      <p>hello world</p>
+    </div>
+  )
+
+  // return <h1>Options</h1>
 }
 
 ;(async () => {
