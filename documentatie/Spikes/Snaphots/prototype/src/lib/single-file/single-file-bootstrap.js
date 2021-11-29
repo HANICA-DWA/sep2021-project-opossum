f348ac555583a279d401a9cfbc8eba2993d3f736
Mark Jansen <49095667+mjqnsen@users.noreply.github.com>
2021-11-29 16:38:50 +0100

    Spike snapshots and prototype (#59)
    * added project
    
    * working on prototype
    
    * working on solution for single file html
    
    * prototype
    
    * working on the snapshot editor
    
    * snapshot onderzoek editor screen
    
    * write down snapshot spike
    
    * Apply suggestions from code review
    
    Co-authored-by: Ruben <47324044+RubenEppink@users.noreply.github.com>
    
    * Update documentatie/Spikes/Snaphots/readme.md
    
    Co-authored-by: Ruben <47324044+RubenEppink@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: ilivss <iliass@iek.dev>
    
    Co-authored-by: Ruben <47324044+RubenEppink@users.noreply.github.com>
    Co-authored-by: ilivss <iliass@iek.dev>

---
+++ documentatie/Spikes/Snaphots/prototype/src/lib/single-file/single-file-bootstrap.js
@@ -0,0 +1,31 @@
+import * as frameTree from "./processors/frame-tree/content/content-frame-tree.js";
+import * as serializer from "./modules/html-serializer.js";
+import {
+	COMMENT_HEADER,
+	ON_BEFORE_CAPTURE_EVENT_NAME,
+	ON_AFTER_CAPTURE_EVENT_NAME,
+	initUserScriptHandler,
+	preProcessDoc,
+	postProcessDoc,
+	getShadowRoot
+} from "./single-file-helper.js";
+
+const processors = { frameTree };
+const helper = {
+	COMMENT_HEADER,
+	ON_BEFORE_CAPTURE_EVENT_NAME,
+	ON_AFTER_CAPTURE_EVENT_NAME,
+	preProcessDoc,
+	postProcessDoc,
+	serialize(doc, compressHTML) {
+		return serializer.process(doc, compressHTML);
+	},
+	getShadowRoot
+};
+
+initUserScriptHandler();
+
+export {
+	helper,
+	processors
+};
---
+++ documentatie/Spikes/Snaphots/prototype/webpack/webpack.common.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const srcDir = path.join(__dirname, '..', 'src')
+
+module.exports = {
+  entry: {
+    popup: path.join(srcDir, 'views', 'popup.jsx'),
+    options: path.join(srcDir, 'views', 'options.jsx'),
+    editor: path.join(srcDir, 'views', 'editor.jsx'),
+  },
+  output: {
+    path: path.join(__dirname, '../dist/js'),
+    filename: '[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks(chunk) {
+        return chunk.name !== 'background'
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './public', to: '../'},
+        { from: 'manifest.json', to: '../' },
+        { from: './src/extension', to: '../extension' },
+        { from: './src/common', to: '../common' },
+        { from: './src/lib', to: '../lib' },
+        { from: './_locales', to: '../_locales' },
+      ],
+      options: {},
+    }),
+  ],
+}
