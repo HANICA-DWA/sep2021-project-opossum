3b6091c0737cf68e16d75cfe32b0005db38df237
Ruben Eppink <ruben_eppink@live.nl>
2021-11-29 13:06:50 +0100

    update xpath vs css demo

--- documentatie/Spikes/CSS vs XPath/xpath-demo/src/App.js
+++ documentatie/Spikes/CSS vs XPath/xpath-demo/src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import getXPath from 'get-xpath';
 
 function App() {
   const [selector, setSelector] = useState('');
 
+  useEffect(() => {
   document.querySelectorAll('*').forEach(element => {
-    element.addEventListener('mouseover', event => {
+    element.addEventListener('click', event => {
       //if event.target is body or html, don't change the border
-      if (event.target.tagName === 'BODY' || event.target.tagName === 'HTML') {
+      if (event.target.tagName === 'BODY' || event.target.tagName === 'HTML' || event.target !== element) {
         return;
       }
+     console.time('evaluate');
+      //console.time('getXPath');
+    
       const xpath = getXPath(event.target);
+
+     // console.timeEnd('getXPath');
+     
+
+      
       const el = document.evaluate(
         xpath,
         document,
@@ -20,6 +29,9 @@ function App() {
         XPathResult.FIRST_ORDERED_NODE_TYPE,
         null
       ).singleNodeValue;
+
+      console.timeEnd('evaluate')
+
       // change the border of the element
       el.style.border = '1px solid red';
 
@@ -33,9 +45,10 @@ function App() {
       event.target.style.border = 'none';
     });
   });
+}, []);
   return (
     <>
-      <h1>selector: {selector}</h1>
+      <h1>xpath selector: {selector}</h1>
       <h1>
         My First <b>Heading</b>
       </h1>
