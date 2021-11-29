65cc45d0b1863d618e1389d3aed5c3cdf83e5b8a
Iliass <iliass@iek.dev>
2021-11-29 13:41:55 +0100

    Eslint everything

--- wcag-tool/backend/tests/models/annotation.model.test.js
+++ wcag-tool/backend/tests/models/annotation.model.test.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose');
-const db = require('./db');
-const { Annotation } = require('../../src/models/annotation.model');
+const mongoose = require('mongoose')
+const db = require('./db')
+const { Annotation } = require('../../src/models/annotation.model')
 
 beforeAll(async () => {
-  await db.setupDB();
-});
+  await db.setupDB()
+})
 
 afterEach(async () => {
-  await db.dropCollections();
-});
+  await db.dropCollections()
+})
 
 afterAll(async () => {
-  await db.dropDB();
-});
+  await db.dropDB()
+})
 
 describe('Annotation model', () => {
   test('Create annotation without required fields should fail', async () => {
     // Arrange
-    const annotation = new Annotation({});
-    let error;
+    const annotation = new Annotation({})
+    let error
 
     // Act
     try {
-      await annotation.save();
+      await annotation.save()
     } catch (_error) {
-      error = _error;
+      error = _error
     }
 
     // Assert
-    expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
-    expect(error.errors.title).toBeDefined();
-    expect(error.errors.description).toBeDefined();
-    expect(error.errors.selector).toBeDefined();
-  });
-});
+    expect(error).toBeInstanceOf(mongoose.Error.ValidationError)
+    expect(error.errors.title).toBeDefined()
+    expect(error.errors.description).toBeDefined()
+    expect(error.errors.selector).toBeDefined()
+  })
+})
