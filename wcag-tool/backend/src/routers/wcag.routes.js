9415198748baf9c1379976e210ded309dfa0da16
Iliass <iliass@iek.dev>
2021-11-30 10:03:19 +0100

    Add wcag routes (only read all)

--- wcag-tool/backend/src/routers/wcag.routes.js
+++ wcag-tool/backend/src/routers/wcag.routes.js
@@ -1,13 +1,55 @@
 const { Router } = require('express')
-const { Wcag } = require('../models')
+const { SuccessCriterium, Principle, Guideline } = require('../models')
 
 const router = new Router()
 
-router.get('/wcag', async (req, res, next) => {
+router.get('/wcag/principles', async (req, res, next) => {
   try {
-    const wcag = await Wcag.find({}).exec()
+    // TODO: Welke data is nodig?
+    const principles = await Principle.find(
+      {},
+      {
+        _id: 0,
+      }
+    )
 
-    return res.json(wcag)
+    return res.json(principles)
+  } catch (err) {
+    return next(err)
+  }
+})
+
+router.get('/wcag/guidelines', async (req, res, next) => {
+  try {
+    // TODO: Welke dat is nodig?
+    const guidelines = await Guideline.find(
+      {},
+      {
+        _id: 0,
+        techniques: 0,
+      }
+    )
+
+    return res.send(guidelines)
+  } catch (err) {
+    return next(err)
+  }
+})
+
+router.get('/wcag/successcritera', async (req, res, next) => {
+  try {
+    // TODO: Welke data is nodig?
+    const successcriteria = await SuccessCriterium.find(
+      {},
+      {
+        _id: 0,
+        principle: 0,
+        guideline: 0,
+        techniques: 0,
+      }
+    ).exec()
+
+    return res.json(successcriteria)
   } catch (err) {
     return next(err)
   }
