7f25caa2e88c9cd1d40cf392f48317d2feab16f8
Iliass <iliass@iek.dev>
2021-12-01 11:32:31 +0100

    Add integration tests for wcag endpoints

--- wcag-tool/backend/tests/routes/wcag.routes.test.js
+++ wcag-tool/backend/tests/routes/wcag.routes.test.js
@@ -1,30 +1,121 @@
-const mongoose = require('mongoose')
-const request = require('supertest')
+/* eslint-disable func-names, prefer-arrow-callback */
+const chai = require('chai')
+const chaiHttp = require('chai-http')
 const { app } = require('../../src/app')
+const setup = require('./setup')
 
-beforeAll(async () => {
-  await mongoose.connect(process.env.TEST_MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+chai.use(chaiHttp)
+const { expect, request } = chai
+
+describe('WCAG Endpoints', function () {
+  before(async function () {
+    await setup.before()
   })
-})
 
-afterAll(async () => {
-  await mongoose.disconnect()
-  await mongoose.connection.close()
-})
+  after(async function () {
+    await setup.after()
+  })
 
-describe('WCAG Endpoints', () => {
-  test('Get principles', async () => {
-    // Arrange
+  describe('Principle Endpoints', function () {
+    it('GET all principles', async function () {
+      const response = await request(app).get('/v1/wcag/principles')
 
-    // Act
-    const res = await request(app).get('/v1/wcag/principles')
+      expect(response.status).to.equal(200)
+      response.body.forEach((el) => {
+        expect(el).contains.keys('principleId', 'num', 'handle', 'title')
+      })
+    })
 
-    // Assert
-    expect(res.statusCode).toEqual(200)
-    res.body.forEach((el) =>
-      expect(el).toContainKeys(['principleId', 'num', 'handle', 'title'])
-    )
+    it('GET principle by principleId', async function () {
+      const response = await request(app).get(
+        '/v1/wcag/principles/WCAG2:perceivable'
+      )
+
+      expect(response.status).to.equal(200)
+      expect(response.body).contains.keys(
+        'principleId',
+        'num',
+        'handle',
+        'title'
+      )
+    })
+
+    it('GET principle by principleId with an invalid id should fail', async function () {
+      const response = await request(app).get('/v1/wcag/principles/invalidId')
+
+      expect(response.status).to.equal(404)
+      expect(response.body).to.have.property('message')
+      expect(response.body.message).to.equal('Principle not found!')
+    })
+  })
+
+  describe('Guideline Endpoints', function () {
+    it('GET all guidelines', async function () {
+      const response = await request(app).get('/v1/wcag/guidelines')
+
+      expect(response.status).to.equal(200)
+      response.body.forEach((el) => {
+        expect(el).contains.keys('guidelineId', 'num', 'handle', 'title')
+      })
+    })
+
+    it('GET guideline by guidelineId', async function () {
+      const response = await request(app).get(
+        '/v1/wcag/guidelines/WCAG2:text-alternatives'
+      )
+
+      expect(response.status).to.equal(200)
+      expect(response.body).contains.keys(
+        'guidelineId',
+        'num',
+        'handle',
+        'title'
+      )
+    })
+
+    it('GET guideline by guidelineId with an invalid id should fail', async function () {
+      const response = await request(app).get('/v1/wcag/guidelines/invalidId')
+
+      expect(response.status).to.equal(404)
+      expect(response.body).to.have.property('message')
+      expect(response.body.message).to.equal('Guideline not found!')
+    })
+  })
+
+  describe('SuccesCriterium endpoints', function () {
+    it('GET all success criteria', async function () {
+      const response = await request(app).get('/v1/wcag/successcriteria')
+
+      expect(response.status).to.equal(200)
+      response.body.forEach((el) => {
+        expect(el).contains.keys('successCriteriumId', 'num', 'handle', 'title')
+      })
+    })
+
+    it('GET success criteria by successCriteriumId', async function () {
+      const response = await request(app).get(
+        `/v1/wcag/successcriteria/WCAG2:non-text-content`
+      )
+
+      expect(response.status).to.equal(200)
+      expect(response.body).contains.keys(
+        'successCriteriumId',
+        'num',
+        'handle',
+        'title',
+        'level',
+        'details'
+      )
+    })
+
+    it('GET success criteria by successCriteriumId with an invalid id should fail', async function () {
+      const response = await request(app).get(
+        `/v1/wcag/successcriteria/invalidId`
+      )
+
+      expect(response.status).to.equal(404)
+      expect(response.body).to.have.property('message')
+      expect(response.body.message).to.equal('Success criterium not found!')
+    })
   })
 })
