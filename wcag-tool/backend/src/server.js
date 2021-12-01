c65d34a66c9eaf58197907ad9d96be7d70882958
Iliass <iliass@iek.dev>
2021-12-01 11:13:08 +0100

    Separate server from app

--- wcag-tool/backend/src/server.js
+++ wcag-tool/backend/src/server.js
@@ -1,23 +1,23 @@
-require('dotenv').config()
+require('dotenv/config')
 const mongoose = require('mongoose')
 const { app } = require('./app')
 
 // WEBSOCKET CODE
 // TODO
 
-// START SERVER
+// Connect to MongoDB
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) throw new Error('Could not connect to MongoDB!')
+    /* eslint-disable-next-line no-console */
+    console.log('Connected to MongoDB!')
+  }
+)
+
+// Start server
 app.listen(process.env.PORT, async () => {
   /* eslint-disable-next-line no-console */
   console.log(`Server started listening on port ${process.env.PORT}`)
-
-  // Connect to MongoDB
-  await mongoose.connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-      if (err) throw new Error('Could not connect to MongoDB!')
-      /* eslint-disable-next-line no-console */
-      console.log('Connected to MongoDB!')
-    }
-  )
 })
