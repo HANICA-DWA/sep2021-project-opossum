e1deff47a421a8f147e3cee07d5f0c984ea541ce
Steve <stefanoudelohuis2000@gmail.com>
2021-12-03 11:09:06 +0100

    Feedback verwerkt + Merge develop

--- wcag-tool/chrome-extension/src/views/components/editor/AnnotationDetailSlider.jsx
+++ wcag-tool/chrome-extension/src/views/components/editor/AnnotationDetailSlider.jsx
@@ -11,7 +11,7 @@ import {
 } from '../../../services/annotationSlice'
 import {
   selectorDetailSliderIsOpen,
-  // setCreateSliderIsOpen,
+  setCreateSliderIsOpen,
   setDetailSliderIsOpen,
   setListSliderIsOpen,
 } from '../../../services/slidersSlice'
@@ -49,21 +49,21 @@ const AnnotationDetailSlider = function () {
       title={
         <div className="grid grid-cols-6 rounded-l items-center px-5">
           <div className="col-span-5">
-            <p title={annotation.title} className="text-base font-medium text-gray-900">
-              {truncateStringAndCapitalize(70, annotation.title)}
+            <p title={annotation?.title} className="text-base font-medium text-gray-900">
+              {truncateStringAndCapitalize(70, annotation?.title)}
             </p>
           </div>
-          <div>
+          <div className="flex justify-end">
             <IconButton
               className="pencilIcon p-0.5 mr-1"
               onClick={() => {
                 dispatch(setSelectedAnnotation(annotation))
-                dispatch(setEditSliderIsOpen(true))
+                dispatch(setCreateSliderIsOpen(true))
               }}
             />
             <IconButton
               className="crossIcon p-0.5 ml-1"
-              onClick={() => { 
+              onClick={() => {
                 dispatch(setDetailSliderIsOpen(false))
                 dispatch(setListSliderIsOpen(true))
                 dispatch(unsetSelectedAnnotation())
@@ -74,7 +74,7 @@ const AnnotationDetailSlider = function () {
         </div>
       }
     >
-      <div className="pl-5 pr-8 py-5">{annotation.description}</div>
+      <div className="pl-5 pr-8 py-5">{annotation?.description}</div>
       {/* <div className="flex justify-center">
         <footer className="footer fixed bottom-0 pt-1 py-2  border-b-2">
           <button
