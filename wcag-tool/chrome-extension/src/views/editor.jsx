284306dd4f8062cc4071fde8b81d97e9b797ba28
Mark Jansen <MJ.Jansen4@student.han.nl>
2021-12-05 15:20:16 +0100

    added snapshot prototype files

---
+++ wcag-tool/chrome-extension/src/views/editor.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../css/styles.css'
+import './editor.css'
+import './editor.js'
+
+const Editor = () => {
+  const editorElement = document.querySelector('.editor')
+
+  return (
+    <div className={"toolbar"}>
+      <button onClick={(e)=> {
+        console.log("clicked")
+        editorElement.contentWindow.postMessage(JSON.stringify({method: 'elementSelect'}), '*')
+        editorElement.contentWindow.focus()
+      }}>Test</button>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Editor />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
