bd568008e4fba4e85d0e3a6fa80b7561f7b1325b
Ruben <47324044+RubenEppink@users.noreply.github.com>
2021-12-06 16:37:20 +0100

    Task 78 delete annotation server side (#80)
    * Move components folder
    
    * Add seperate form + wcag selector
    
    * Add successcriterium to backend snapshot + annotation models
    
    * Co-authored-by: JS Master Ruben <RubenEppink@users.noreply.github.com>
    
    * delete annotation server-side
    
    * finished unit test
    
    * wcag selector fix
    
    * Remove excess console.logs
    
    * Fix test
    
    * Test fix
    
    * removed comments
    
    * fixed tests
    
    * Update wcag-tool/backend/src/routers/annotation.routes.js
    
    Co-authored-by: Mark Jansen <49095667+mjqnsen@users.noreply.github.com>
    
    * Update wcag-tool/chrome-extension/src/components/editor/AnnotationListItem.jsx
    
    Co-authored-by: Mark Jansen <49095667+mjqnsen@users.noreply.github.com>
    
    * lint fixes
    
    * fixed merge issue
    
    * fixed error handling
    
    * fixed tests
    
    Co-authored-by: Iliass <iliass@iek.dev>
    Co-authored-by: Ruben <RubenEppink@users.noreply.github.com>
    Co-authored-by: Mark Jansen <49095667+mjqnsen@users.noreply.github.com>

--- wcag-tool/backend/src/routers/annotation.routes.js
+++ wcag-tool/backend/src/routers/annotation.routes.js
@@ -6,16 +6,15 @@ const router = new Router()
 // add new annotation to the list of annotations in a snapshot
 router.post('/snapshots/:snapshotId/annotations', async (req, res, next) => {
   try {
-    console.log(req.body)
-
     const { successCriterium, title, description, selector } = req.body
 
     const annotation = await req.snapshot.addAnnotation(
-      successCriterium,
       title,
       description,
-      selector
+      selector,
+      successCriterium
     )
+
     if (!annotation) return next({ code: 500, message: 'Annotation could not be added' })
 
     return res.status(201).json(annotation)
@@ -49,6 +48,18 @@ router.patch('/snapshots/:snapshotId/annotations/:annotationId', async (req, res
     if (!annotation) return next({ code: 500, message: 'Annotation could not be updated' })
 
     return res.json(annotation)
+  } catch (err) {
+    return next(err)
+  }
+})
+
+// delete annotation from a snapshot
+router.delete('/snapshots/:snapshotId/annotations/:annotationId', async (req, res, next) => {
+  try {
+    const deletedAnnotation = await req.snapshot.deleteAnnotation(req.params.annotationId)
+    if (!deletedAnnotation) return next({ code: 404, message: 'Could not find annotation' })
+
+    return res.json(deletedAnnotation)
   } catch (err) {
     return next(err)
   }
