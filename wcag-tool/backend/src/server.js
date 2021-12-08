7c94f45ce475be5f522149e76b5d6263b541baf8
ilivss <iliass@iek.dev>
2021-12-08 09:40:34 +0100

    Add yjs websocket server (#88)
    * Add yjs websocket server

--- wcag-tool/backend/src/server.js
+++ wcag-tool/backend/src/server.js
@@ -1,12 +1,26 @@
 require('dotenv/config')
-require('./database')
-const { app } = require('./app')
 
-// WEBSOCKET CODE
-// TODO
+const { createServer } = require('http')
+
+const { app } = require('./app')
+const { wsServer } = require('./websocket')
+
+const httpServer = createServer(app)
+
+// Database code
+require('./database')
+
+// Websocket code
+httpServer.on('upgrade', (req, socket, head) => {
+  // TODO authenticate requests here!
+
+  wsServer.handleUpgrade(req, socket, head, (ws, _req) => {
+    wsServer.emit('connection', ws, req)
+  })
+})
 
 // Start server
-app.listen(process.env.PORT, async () => {
+httpServer.listen(process.env.PORT, async () => {
   /* eslint-disable-next-line no-console */
   console.log(`Server started listening on port ${process.env.PORT}`)
 })
