941c108e336411bd5a7fe381c9616467b42fa654
Iliass <iliass@iek.dev>
2021-12-09 16:38:03 +0100

    Add awareness

--- wcag-tool/chrome-extension/src/components/editor/App.jsx
+++ wcag-tool/chrome-extension/src/components/editor/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useYjs } from '../../hooks'
+
 import OverlayButton from './OverlayButton'
 import AnnotationListSlider from './AnnotationListSlider'
 import CreateAndEditAnnotationSlider from './CreateAndEditAnnotationSlider'
 import AnnotationDetailSlider from './AnnotationDetailSlider'
+import Awareness from './Awareness'
 
 const App = () => {
+  const { ydoc, provider } = useYjs()
   return (
     <>
+      <Awareness provider={provider} clientId={ydoc.clientID} />
       <OverlayButton />
       <AnnotationListSlider />
       <AnnotationDetailSlider />
---
+++ wcag-tool/chrome-extension/src/components/editor/Awareness.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react'
+
+import { useSliders } from '../../hooks'
+
+export function FirstLetterAndCapitalize(str = '') {
+  if (str?.length > 0) return str.charAt(0).toUpperCase()
+
+  return ''
+}
+
+const Awareness = ({ provider, clientId }) => {
+  // eslint-disable-next-line no-empty-pattern
+  const [{}, { anySliderIsOpen, elementSelectorIsOpen }] = useSliders()
+  const position = anySliderIsOpen ? 'transform-to-right' : 'transform-to-left'
+  const invisible = elementSelectorIsOpen ? 'transparent-and-hide' : ''
+
+  const [clients, setClients] = useState([])
+
+  useEffect(() => {
+    if (provider) {
+      // Listen to awareness changes
+      provider.awareness.on('change', () => {
+        const _clients = []
+        provider.awareness.getStates().forEach((state) => {
+          if (state.user) _clients.push({ ...state.user })
+        })
+        setClients(_clients)
+      })
+
+      const names = [
+        'Bob',
+        'James',
+        'Jessie',
+        'Rob',
+        'Harry',
+        'Henk',
+        'Jan',
+        'Alfred',
+        'Xenos',
+        'Siegmeyer',
+        'Ifrit',
+        'Peter',
+        'May',
+        'Yvonne',
+        'Frank',
+        'Gerda',
+        'Jolanda',
+      ]
+      const colours = ['red', 'yellow', 'green', 'violet', 'orange', 'pink', 'gray', 'lime']
+
+      // Set awareness information
+      provider.awareness.setLocalStateField('user', {
+        id: clientId,
+        name: names[Math.floor(Math.random() * names.length)], // TODO: Change for real values
+        color: colours[Math.floor(Math.random() * colours.length)], // TODO: Change for real values
+      })
+    }
+
+    // Cleanup after unmounting component
+    return () => {
+      provider.awareness.off('change')
+    }
+  }, [provider])
+
+  return (
+    <div
+      className={`flex flex-col absolute top-12 p-4 pr-8 pb-8 font-poppins ${position} ${invisible}`}
+    >
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          title={client.name}
+          className="flex justify-center items-center rounded-full my-1 h-8 w-8 opacity-60 hover:opacity-100"
+          style={{ backgroundColor: client.color }}
+        >
+          <p className="text-center text-sm font-poppins-semi cursor-default">
+            {FirstLetterAndCapitalize(client.name)}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Awareness
