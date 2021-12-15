9773f0cd5779e299af2b03441c0f0fb4334b6296
Iliass <iliass@iek.dev>
2021-12-15 17:38:42 +0100

    Add fixes according to tests

--- wcag-tool/backend/src/middleware/errorHandler.js
+++ wcag-tool/backend/src/middleware/errorHandler.js
@@ -3,9 +3,13 @@ const errorHandler = (err, req, res, next) => {
   /* eslint-disable-next-line no-console */
   if (process.env.NODE_ENV !== 'test') console.log('Error: ', err)
 
-  if (err.code && err.message) return res.status(err.code).json({ message: err.message })
+  if (err.code && err.message) return res.status(err.code).json({ message: err.message }) // Custom errors
+  if (err.errors) {
+    // Mongoose validation errors
+    return res.status(400).send()
+  }
 
-  return res.status(500).send('Whoops, something went wrong!')
+  return res.status(500).send()
 }
 
 module.exports = { errorHandler }
