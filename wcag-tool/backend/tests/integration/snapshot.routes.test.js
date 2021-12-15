2eab50201c332ba9e48875fcce491ba88d195a6f
Iliass <iliass@iek.dev>
2021-12-15 13:26:41 +0100

    Add snapshot tests

--- wcag-tool/backend/tests/integration/snapshot.routes.test.js
+++ wcag-tool/backend/tests/integration/snapshot.routes.test.js
@@ -1,9 +1,12 @@
 /* eslint-disable func-names, prefer-arrow-callback,no-unused-expressions */
 const fs = require('fs')
+const path = require('path')
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const { app } = require('../../src/app')
 const setup = require('./setup')
+const { seedSnapshots } = require('../../src/utils/seed')
+const { Snapshot } = require('../../src/models')
 
 chai.use(chaiHttp)
 const { expect, request } = chai
@@ -17,16 +20,18 @@ describe('Snapshot Endpoints', function () {
     await setup.after()
   })
 
-  describe('Create', function () {
+  afterEach(async function () {
+    await setup.afterEach()
+  })
+
+  describe('POST snapshot', function () {
     it('Post snapshot successfully', async function () {
-      const response = await await request(app)
+      const response = await request(app)
         .post('/v1/snapshots')
         .type('form')
-        .attach('file', fs.readFileSync('tests/routes/snapshot.html'), 'filename.html')
+        .attach('file', fs.readFileSync(path.join(__dirname, 'snapshot.html')), 'filename.html')
         .field('name', 'untitled snapshot')
         .field('domain', 'test.com')
-
-      console.log('response', response.body)
 
       expect(response.status).equals(201)
       expect(response.body._id).to.exist
@@ -35,26 +40,146 @@ describe('Snapshot Endpoints', function () {
     })
 
     it('Post snapshot without a file should fail', async function () {
-      const response = await await request(app)
+      const response = await request(app)
         .post('/v1/snapshots')
         .type('form')
         .field('name', 'untitled snapshot')
         .field('domain', 'test.com')
-
-      console.log('response', response.body)
 
       expect(response.status).equals(400)
       expect(response.body.message).equals('File not provided!')
     })
 
     it('Post snapshot without required fields should fail', async function () {
-      const response = await await request(app)
+      const response = await request(app)
         .post('/v1/snapshots')
         .type('form')
-        .attach('file', fs.readFileSync('tests/routes/snapshot.html'), 'filename.html')
+        .attach('file', fs.readFileSync(path.join(__dirname, 'snapshot.html')), 'filename.html')
 
-      expect(response.status).equals(500)
-      // expect(response.body.message).equals('File not provided!')
+      expect(response.status).equals(400)
+      expect(response.body.message.endsWith('not provided!')).equals(true)
+    })
+  })
+
+  describe('GET snapshots', function () {
+    it('Get snapshots without pagination params should return all snapshots', async function () {
+      await seedSnapshots()
+
+      const response = await request(app).get('/v1/snapshots')
+
+      expect(response.status).equals(200)
+      expect(response.body.length).equals(10)
+    })
+
+    it('Get snapshots with pagination params successfully', async function () {
+      await seedSnapshots()
+
+      const response = await request(app).get('/v1/snapshots?page=1&limit=4')
+      const response2 = await request(app).get('/v1/snapshots?page=2&limit=4')
+      const response3 = await request(app).get('/v1/snapshots?limit=10')
+
+      expect(response.status).equals(200)
+      expect(response.body[0].name.endsWith('0')).equals(true)
+      expect(response.body[3].name.endsWith('3')).equals(true)
+
+      expect(response2.status).equals(200)
+      expect(response2.body[0].name.endsWith('4')).equals(true)
+      expect(response2.body[3].name.endsWith('7')).equals(true)
+
+      expect(response3.status).equals(200)
+      expect(response3.body[0].name.endsWith('0')).equals(true)
+      expect(response3.body[9].name.endsWith('9')).equals(true)
+    })
+
+    it('Get snapshots with invalid pagination params should fail', async function () {
+      await seedSnapshots()
+
+      const response = await request(app).get('/v1/snapshots?page=1')
+      const response2 = await request(app).get('/v1/snapshots?limit=0')
+      const response3 = await request(app).get('/v1/snapshots?page=1&limit=0')
+
+      expect(response.status).equals(400)
+      expect(response.body.message).equals('Invalid pagination parameters!')
+
+      expect(response2.status).equals(400)
+      expect(response2.body.message).equals('Invalid pagination parameters!')
+
+      expect(response3.status).equals(400)
+      expect(response3.body.message).equals('Invalid pagination parameters!')
+    })
+
+    it('Get snapshot with id successfully', async function () {
+      const dummySnapshot = await new Snapshot({
+        name: 'dummy snapshot',
+        domain: 'test.com',
+        filename: 'testfile',
+      }).save()
+
+      const response = await request(app).get(`/v1/snapshots/${dummySnapshot._id}`)
+
+      expect(response.status).equals(200)
+      expect(response.body._id).equals(dummySnapshot._id.toString())
+      expect(response.body.name).equals(dummySnapshot.name)
+      expect(response.body.domain).equals(dummySnapshot.domain)
+      expect(response.body.filename).equals(dummySnapshot.filename)
+    })
+
+    it('Get snapshot with non existing id should fail', async function () {
+      const response = await request(app).get('/v1/snapshots/61b7284ded4084fd77ced98b')
+
+      expect(response.status).equals(404)
+      expect(response.body.message).equals('Snapshot not found!')
+    })
+  })
+
+  describe('PUT snapshots', function () {
+    it('Put snapshot successfully', async function () {
+      const dummySnapshot = await new Snapshot({
+        name: 'dummy snapshot',
+        domain: 'test.com',
+        filename: 'testfile',
+      }).save()
+
+      const response = await request(app).put(`/v1/snapshots/${dummySnapshot._id}`).send({
+        name: 'edited name',
+        domain: 'editeddomain.com',
+      })
+
+      expect(response.status).equals(200)
+      expect(response.body.name).equals('edited name')
+      expect(response.body.domain).equals('editeddomain.com')
+    })
+
+    it('Put snapshot with non existing id should fail', async function () {
+      const response = await request(app).put('/v1/snapshots/61b7284ded4084fd77ced98b')
+
+      expect(response.status).equals(404)
+      expect(response.body.message).equals('Snapshot not found!')
+    })
+  })
+
+  describe('DELETE snapshots', function () {
+    it('Delete snapshot successfully', async function () {
+      const dummySnapshot = await new Snapshot({
+        name: 'dummy snapshot',
+        domain: 'test.com',
+        filename: 'testfile',
+      }).save()
+
+      const response = await request(app).delete(`/v1/snapshots/${dummySnapshot._id}`)
+
+      expect(response.status).equals(200)
+      expect(response.body._id).equals(dummySnapshot._id.toString())
+      expect(response.body.name).equals('dummy snapshot')
+      expect(response.body.domain).equals('test.com')
+      expect(response.body.filename).equals('testfile')
+    })
+
+    it('Delete snapshot with non existing id should fail', async function () {
+      const response = await request(app).delete('/v1/snapshots/61b7284ded4084fd77ced98b')
+
+      expect(response.status).equals(404)
+      expect(response.body.message).equals('Snapshot not found!')
     })
   })
 })
