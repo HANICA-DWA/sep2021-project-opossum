de90b2f92ae86757363cf49e7010b08cacbc4bc2
Iliass <iliass@iek.dev>
2021-12-21 14:03:17 +0100

    Add axecore violation to annotation mapper

--- documentatie/Spikes/axe-core/main.js
+++ documentatie/Spikes/axe-core/main.js
@@ -1,7 +1,31 @@
+function axecoreAnnotationMapper(violations) {
+  // Axe Core does not use WCAG2.1! So we cannot use their rules in our annotation model...
+  // https://github.com/dequelabs/axe-core/blob/master/doc/rule-descriptions.md
+
+  const annotations = []
+
+  violations.forEach((v) => {
+    v.nodes.forEach((n) => {
+      annotations.push({
+        title: v.help,
+        description: v.description,
+        selector: n.target,
+        labels: [n.impact, 'Auto Analysis'],
+        draft: true,
+      })
+    })
+  })
+
+  return annotations
+}
+
 const button1 = document.getElementById('button1')
 button1.addEventListener('click', async function () {
   const iframe = document.getElementById('iframe')
-  const result = await iframe.contentWindow.analyse()
-  console.log(result)
+
+  const violations = await iframe.contentWindow.analyse()
+  const annotations = axecoreAnnotationMapper(violations)
+
+  annotations.forEach((el) => console.log(el))
   alert('Check console for result! (Ctrl+Shift+K)')
 })
