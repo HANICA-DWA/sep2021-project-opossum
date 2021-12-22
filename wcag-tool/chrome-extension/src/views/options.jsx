5a074633a8595be6d7b0de4a858c5d659d7f4846
Ruben <47324044+RubenEppink@users.noreply.github.com>
2021-12-22 14:13:03 +0100

    open options page (#135)
    * set username in option menu
    
    * lint fixes
    
    * styling
    
    * Auto stash before merge of "task/120-options-screen" and "origin/task/120-options-screen"
    
    * chnages
    
    * open options page
    
    * lint fix
    
    * Update wcag-tool/chrome-extension/src/components/popup/header.jsx
    
    Co-authored-by: Mark Jansen <49095667+mjqnsen@users.noreply.github.com>
    
    Co-authored-by: Mark Jansen <MJ.Jansen4@student.han.nl>
    Co-authored-by: Mark Jansen <49095667+mjqnsen@users.noreply.github.com>

--- wcag-tool/chrome-extension/src/views/options.jsx
+++ wcag-tool/chrome-extension/src/views/options.jsx
@@ -1,10 +1,95 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from '../services/store'
+import '../css/styles.css'
 
 const Options = function () {
-  return <h1>Options</h1>
+  const [username, setUsername] = useState('')
+  const [saved, setSaved] = useState(false)
+
+  useEffect(() => {
+    chrome.storage.sync.get(['username'], (result) => {
+      setUsername(result.username)
+    })
+  }, [])
+
+  const saveOptions = () => {
+    chrome.storage.sync.set(
+      {
+        username,
+      },
+      () => {
+        setSaved(true)
+      }
+    )
+  }
+  return (
+    <div className="m-6">
+      <h1 className="text-xl font-poppins mb-3">Options</h1>
+      <label className="block font-poppins text-gray-700 text-sm font-bold mb-1" htmlFor="username">
+        Username
+      </label>
+      <div className="grid grid-cols-2 -ml-2">
+        <input
+          className="shadow font-poppins appearance-none border border-gray-300 rounded-l-lg py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
+          id="username"
+          type="text"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value)
+            if (saved) {
+              setSaved(false)
+            }
+          }}
+          placeholder="Username"
+        />
+        <div>
+          <button
+            disabled={!username || saved}
+            onClick={saveOptions}
+            type="button"
+            className={`py-1 px-4 inline-grid grid-flow-col grid-cols-2  ${
+              saved ? 'bg-green-600 cursor-default' : 'bg-blue-600 hover:bg-blue-700 '
+            } text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-r-lg`}
+          >
+            {!saved ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 inline"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 inline"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+            )}
+            {saved ? 'Saved' : 'Save'}
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(
