3d89d86bfdcad6d4d36ba4d3e537e302d5a64e94
Iliass <iliass@iek.dev>
2021-12-22 16:37:15 +0100

    Lint and build fix

--- wcag-tool/chrome-extension/postinstall.js
+++ wcag-tool/chrome-extension/postinstall.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.copyFile(
-  path.join(__dirname, 'node_modules', 'axe-core', 'axe.min.js'),
-  path.join(__dirname, 'dist', 'js', 'axe.min.js'),
-  (err) => {
-    if (err) throw err
-    console.log('node_modules/axe-core/axe-core.min.js was copied to dist/js/axe.min.js')
-  }
+fs.mkdir(
+  path.join('dist', 'js'),
+  { recursive: true },
+  fs.copyFile(
+    path.join(__dirname, 'node_modules', 'axe-core', 'axe.min.js'),
+    path.join(__dirname, 'dist', 'js', 'axe.min.js'),
+    (err) => {
+      if (err) throw err
+      console.log('node_modules/axe-core/axe-core.min.js was copied to dist/js/axe.min.js')
+    }
+  )
 )
