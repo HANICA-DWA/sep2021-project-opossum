e968af8dd3d8c0c8031b91664a00f52e355bfc43
Iliass <iliass@iek.dev>
2022-01-14 11:14:04 +0100

    fix

--- wcag-tool/chrome-extension/src/components/editor/RichTextEditor.jsx
+++ wcag-tool/chrome-extension/src/components/editor/RichTextEditor.jsx
@@ -18,17 +18,19 @@ const RichTextEditor = function ({ field, placeholder, selectedAnnotationId }) {
   // Refs
   const quillRef = useRef(undefined)
   const reactQuillRef = useRef(undefined)
-  const cursorColor = useRef(getRandomColor())
 
   const attachRefs = () => {
     if (typeof reactQuillRef.current.getEditor !== 'function') return
     quillRef.current = reactQuillRef.current.getEditor()
   }
 
-  provider.awareness.setLocalStateField('user', {
-    name: options.username || 'Unknown user',
-    color: cursorColor.current,
-  })
+  useEffect(() => {
+    provider.awareness.setLocalStateField('user', {
+      name: options.username || 'Unknown user',
+      color: getRandomColor(),
+    })
+  
+  }, [])
 
   // Quill modules
   const modules = {
