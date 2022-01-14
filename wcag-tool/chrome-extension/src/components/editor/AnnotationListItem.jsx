4eab16abfccf434edd58242019bfbc5eda320fef
Ruben Eppink <ruben_eppink@live.nl>
2022-01-14 16:54:48 +0100

    removed dupe func

--- wcag-tool/chrome-extension/src/components/editor/AnnotationListItem.jsx
+++ wcag-tool/chrome-extension/src/components/editor/AnnotationListItem.jsx
@@ -27,17 +27,6 @@ const AnnotationListItem = function ({ annotation, index }) {
     return vertInView && horInView
   }
 
-  const isElementInViewport = (el) => {
-    const rect = el.getBoundingClientRect()
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight
-    const windowWidth = window.innerWidth || document.documentElement.clientWidth
-
-    const vertInView = rect.top <= windowHeight && rect.bottom >= 0
-    const horInView = rect.left <= windowWidth && rect.right >= 0
-
-    return vertInView && horInView
-  }
-
   return (
     <div
       onMouseEnter={() => dispatch(setHighlightedElementSelector(selector))}
